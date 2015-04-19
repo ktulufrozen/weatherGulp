c6c6c83d05d560074a33182f7be64f5543af16df
Mihai Coros <mihai.coros@gmail.com>
2015-04-19 13:18:18 +0300

    Added css minification for production

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,33 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     runSequence = require('run-sequence'),
+    args = require('yargs').argv,
+    minifycss = require('gulp-minify-css'),
+    concat = require('gulp-concat'),
+
+    baseOutputFolder = 'build',
 
     fileNameBase = 'weather',
     lessFileName = fileNameBase + '.less',
+    cssMinifiedFileName = fileNameBase + '.min.css',
 
     cssFolder = 'css',
-    customCssFolder = cssFolder + '/custom';
+    customCssFolder = cssFolder + '/custom',
+    cssOutputFolder = baseOutputFolder + '/css',
 
+    isProduction = args.prod;
 
 gulp.task('lessToCss', function() {
-    var result = gulp.src(customCssFolder + '/' + lessFileName)
-        .pipe(less());
+    gulp.src(customCssFolder + '/' + lessFileName)
+        .pipe(less())
+        .pipe(gulp.dest(customCssFolder));
 
-    return result.pipe(gulp.dest(customCssFolder));
+    if (isProduction) {
+        return gulp.src([cssFolder + '/**/*.css'])
+            .pipe(concat(cssMinifiedFileName))
+            .pipe(minifycss())
+            .pipe(gulp.dest(cssOutputFolder));
+    }
 });
 
 gulp.task('default', function(callback) {
