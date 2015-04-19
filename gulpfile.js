6ebd8058a4c7045846312baa5971c8e69c7c4767
Mihai Coros <mihai.coros@gmail.com>
2015-04-19 13:11:55 +0300

    Added task for less to css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    runSequence = require('run-sequence'),
 
-gulp.task('default', function() {
-    process.stdout.write('Gulp for Weather app started!\n');
+    fileNameBase = 'weather',
+    lessFileName = fileNameBase + '.less',
+
+    cssFolder = 'css',
+    customCssFolder = cssFolder + '/custom';
+
+
+gulp.task('lessToCss', function() {
+    var result = gulp.src(customCssFolder + '/' + lessFileName)
+        .pipe(less());
+
+    return result.pipe(gulp.dest(customCssFolder));
+});
+
+gulp.task('default', function(callback) {
+    runSequence('lessToCss',
+        callback);
 });
